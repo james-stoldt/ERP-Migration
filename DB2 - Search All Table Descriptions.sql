4aba9294b120b078e364ef499505b79a5ebc1b0f
james-stoldt <167944432+james-stoldt@users.noreply.github.com>
2024-07-15 14:26:30 -0400

    fixed formatting

--- DB2 - Search All Table Descriptions.sql
+++ DB2 - Search All Table Descriptions.sql
@@ -1,39 +1,36 @@
 --------------------------------------------------------------------------------------------------------------------------------------
---Author:	James Stoldt
---Date:		July 15, 2024
---DBMS: 	IBM DB2
---------------------------------------------------------------------------------------------------------------------------------------
---Searches through all table name DESCRIPTIONS within all schemas
-
---This is a useful trick to find tables by description when you don't know their names
-
---See https://www.ibm.com/docs/en/i/7.5?topic=views-i-catalog-tables for more information
-
+-- Author:  James Stoldt
+-- Date:    July 15, 2024
+-- DBMS:    IBM DB2
 --------------------------------------------------------------------------------------------------------------------------------------
 
---This example searches for tables in the schema SCHEMA123 that contain the word "order" in their DESCRIPTION (case insensitive):
+-- Searches through all table name DESCRIPTIONS within all schemas
+-- This is a useful trick to find tables by description when you don't know their names
+-- See https://www.ibm.com/docs/en/i/7.5?topic=views-i-catalog-tables for more information
 
---SELECT
---    TABLE_SCHEMA,
---    TABLE_NAME,
---    TABLE_TEXT
+--------------------------------------------------------------------------------------------------------------------------------------
 
---FROM
---    QSYS2.SYSTABLES
- 
---WHERE
---    UPPER(TABLE_TEXT) LIKE UPPER('%order%')
---        AND
---    TABLE_SCHEMA = 'SCHEMA123';
+-- This example searches for tables in the schema SCHEMA123 that contain the word "order" in their DESCRIPTION (case insensitive):
 
---Sample results:
+-- SELECT
+--     TABLE_SCHEMA,
+--     TABLE_NAME,
+--     TABLE_TEXT
+-- FROM
+--     QSYS2.SYSTABLES
+-- WHERE
+--     UPPER(TABLE_TEXT) LIKE UPPER('%order%')
+--         AND
+--     TABLE_SCHEMA = 'SCHEMA123';
 
---TABLE_SCHEMA		TABLE_NAME		TABLE_TEXT
----------------------------------------------------------------
---SCHEMA123			COHEADER		Customer Order Header File
---SCHEMA123			CODETAIL		Customer Order Detail File
---SCHEMA123			POHEADER		Purchase Order Header File
---SCHEMA123			PODETAIL		Purchase Order Detail File
+-- Sample results:
+
+-- TABLE_SCHEMA    TABLE_NAME    TABLE_TEXT
+-- ----------------------------------------------------------------
+-- SCHEMA123       COHEADER      Customer Order Header File
+-- SCHEMA123       CODETAIL      Customer Order Detail File
+-- SCHEMA123       POHEADER      Purchase Order Header File
+-- SCHEMA123       PODETAIL      Purchase Order Detail File
 
 --------------------------------------------------------------------------------------------------------------------------------------
 
@@ -41,10 +38,10 @@ SELECT
     TABLE_SCHEMA,
     TABLE_NAME,
     TABLE_TEXT
-
+	
 FROM
     QSYS2.SYSTABLES
-   
+	
 WHERE
     UPPER(TABLE_TEXT) LIKE UPPER('%TABLE_NAME_KEYWORD_HERE%')
         AND
